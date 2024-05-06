53690ea087cab63d386cdb4d424037b1505a8ac9
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-06 10:36:17 -0400

    factorial

---
+++ debugging/factorial.py
@@ -0,0 +1,12 @@
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1  # Decrement n in each iteration
+    return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
+
