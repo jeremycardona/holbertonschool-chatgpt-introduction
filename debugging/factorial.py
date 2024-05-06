b83858e1b534bfe6ca6b3e39fb76ff6e4533e3d0
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-06 16:32:03 -0400

    added new version

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,9 +5,16 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrement n in each iteration
+        n = n - 1
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if len(sys.argv) != 2:
+    print("Usage: python3 script.py <number>")
+    sys.exit(1)
+
+try:
+    f = factorial(int(sys.argv[1]))
+    print(f)
+except ValueError:
+    print("Please provide a valid integer argument.")
 
